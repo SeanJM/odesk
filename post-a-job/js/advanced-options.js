70d8e9777b2fc4ede2a1636538bc299c86faa4f8
SeanJM <seanjmacisaac@gmail.com>
2012-10-15 17:15:04 -0400

    Worked on feedback rating, making it work as a dragable element anywhere on the page

--- post-a-job/js/advanced-options.js
+++ post-a-job/js/advanced-options.js
@@ -3,24 +3,33 @@ $(function() {
   star.active = function() {
     $('#feedback-score').addClass('active');
 
-    $('html').on('mousemove',function(){});
-    $('#feedback-score .star').each(function() {
-      $(this).on('mousemove',function(e){
-        var offsetPos = e.pageX - $(this).offset().left,
+    $('html').on('mousemove',function(e){
+      if ($('#feedback-score').hasClass('active')) {
+        var offsetPos = e.pageX - $('#feedback-score').offset().left,
             fullWidth = $(this).width(),
-            score;
+            score,
+            star      = {},
+            starPos;
+        for (var i=0;i<$('#feedback-score .star').size();i++) {
+          if (e.pageX > $('#feedback-score .star').eq(i).offset().left) {
+            star = $('#feedback-score .star').eq(i);
+          }
+        }
 
-        if (offsetPos > fullWidth/4 && offsetPos < fullWidth/2) { offsetPos = fullWidth/2; }
-        if (offsetPos > fullWidth/4 && offsetPos < fullWidth/1.5) { offsetPos = fullWidth/2; }
-        if (offsetPos > fullWidth/1.5) { offsetPos = fullWidth; }
-        if (offsetPos <= fullWidth/4) { offsetPos = 0; }
+        fullWidth = star.width();
+        starPos   = e.pageX - star.offset().left;
+
+        if (starPos > fullWidth/4 && starPos < fullWidth/2) { starWidth = fullWidth/2; }
+        if (starPos > fullWidth/4 && starPos < fullWidth/1.5) { starWidth = fullWidth/2; }
+        if (starPos > fullWidth/1.5) { starWidth = fullWidth; }
+        if (starPos <= fullWidth/4) { starWidth = 0; }
         
-        $(this).find('.inner-star').width(offsetPos);
-        $(this).nextAll().each(function() { $(this).find('.inner-star').width(0); });
-        $(this).prevAll().each(function() { $(this).find('.inner-star').width(fullWidth); });
-        score = $(this).prevAll().size() + offsetPos/fullWidth;
+        star.find('.inner-star').width(starWidth);
+        star.nextAll().each(function() { $(this).find('.inner-star').width(0); });
+        star.prevAll().each(function() { $(this).find('.inner-star').width(fullWidth); });
+        score = star.prevAll().size() + starWidth/fullWidth;
         $('#feedback-score').attr('score',score);
-      });
+      }
     });
   };
   $('#feedback-score').on('mousedown',function(){
@@ -28,7 +37,7 @@ $(function() {
   });
   $('html').on('mouseup',function(){
     if ($('#feedback-score').hasClass('active')) {
-      $(this).removeClass('active');
+      $('#feedback-score').removeClass('active');
     }
   });
 });
